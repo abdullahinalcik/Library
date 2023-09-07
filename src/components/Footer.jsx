810d80d1d6043860a4b141ffc150c77cd62ab8d6
abdullahinalcik <130457799+abdullahinalcik@users.noreply.github.com>
2023-09-07 21:41:39 +0300

    edit:Navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,22 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="bg-white  dark:bg-gray-900 m-4">
-      <div class="w-full  mx-auto  md:py-8">
-        asd
-        <hr class="my-6 border-gray-300 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://www.linkedin.com/in/abdullah-inalcik/" target="_blank" class="hover:underline">
-            Abdullah Inalcik
-          </a>
-          . All Rights Reserved.
-        </span>
-      </div>
-    </footer>
+    <footer className="bg-white  dark:bg-gray-900 m-4">
+    <div className="w-full  mx-auto  ">
+      <hr className="my-6 border-gray-300 sm:mx-auto dark:border-gray-700 " />
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2023 {" "}
+        <a
+          href="https://www.linkedin.com/in/abdullah-inalcik/"
+          target="_blank"
+          className="hover:underline"
+        >
+          Abdullah Inalcik
+        </a>
+        . All Rights Reserved.
+      </span>
+    </div>
+  </footer>
   );
 };
 
