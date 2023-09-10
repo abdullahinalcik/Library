a5d14b1b3ee0fd9a992d7f34fe28ccc70b36ed01
abdullahinalcik <130457799+abdullahinalcik@users.noreply.github.com>
2023-09-10 17:49:54 +0300

    edit: card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import SearchBox from "../components/SearchBox";
@@ -6,12 +6,21 @@ import Card from "../components/Card";
 // import { Card } from "flowbite-react";
 
 const Home = () => {
-
   return (
-<>
-<SearchBox/>
-<Card/>
-</>
+    <>
+      <SearchBox />
+      <div className="gap-4 flex max-w-screen-xl flex-wrap  mx-auto justify-center">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
   );
 };
 
